391164c7ccb560d2fb50027955d070a73696f107
Hampus Joakim Borgos <mail@hjnilsson.com>
2021-01-25 08:02:07 +0100

    Initial commit

---
+++ tests/NextNamedRoutes.identifiersInPathname.test.ts
@@ -0,0 +1,20 @@
+import NextNamedRoutes from '.'
+
+describe('NextNamedRoutes.identifiersInPath', () => {
+  it('should split out identifiers', () => {
+    expect(NextNamedRoutes.identifiersInPath('/')).toEqual([])
+
+    expect(NextNamedRoutes.identifiersInPath('/[organizationSlug]')).toEqual(['organizationSlug'])
+
+    expect(NextNamedRoutes.identifiersInPath('/[organizationSlug]/tema/[topicSlug]')).toEqual([
+      'organizationSlug',
+      'topicSlug',
+    ])
+  })
+
+  it('should reject duplicate identifiers', () => {
+    expect(() =>
+      NextNamedRoutes.identifiersInPath('/[organizationSlug]/[organizationSlug]'),
+    ).toThrow()
+  })
+})
---
+++ tests/NextNamedRoutes.injectParamsIntoPath.test.ts
@@ -0,0 +1,18 @@
+import NextNamedRoutes from '.'
+
+describe('NextNamedRoutes.injectParamsIntoPath', () => {
+  it('inject named parameters', () => {
+    expect(NextNamedRoutes.injectParamsIntoPath('/hello', {})).toEqual('/hello')
+
+    expect(
+      NextNamedRoutes.injectParamsIntoPath('/[organizationSlug]', { organizationSlug: 'test-as' }),
+    ).toEqual('/test-as')
+
+    expect(
+      NextNamedRoutes.injectParamsIntoPath('/[organizationSlug]/tema/[topicSlug]', {
+        organizationSlug: 'test-as',
+        topicSlug: 'amazing-Journey',
+      }),
+    ).toEqual('/test-as/tema/amazing-Journey')
+  })
+})
